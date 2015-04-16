944c78c66894dd8d3ab32bf550e2caeac3ee6c20
Jesús Carrete Montaña <jcarrete@gmail.com>
2015-04-16 08:03:46 +0200

    Remove another redundant bit

--- thirdorder_vasp.py
+++ thirdorder_vasp.py
@@ -94,7 +94,7 @@ def write_POSCAR(poscar,filename):
     """
     global hashes
     f=StringIO.StringIO()
-    f.write("1.0\n".format(filename))
+    f.write("1.0\n")
     for i in xrange(3):
         f.write("{0[0]:>20.15f} {0[1]:>20.15f} {0[2]:>20.15f}\n".format(
             (poscar["lattvec"][:,i]*10.).tolist()))
