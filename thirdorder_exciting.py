ca9e11cba5c173f53fb98dc897a2e51e66c8ec68
Nakib Haider <nakib.haider.protik@gmail.com>
2023-11-02 14:18:38 +0100

    Finished the 'sow' part of the workflow.

--- thirdorder_exciting.py
+++ thirdorder_exciting.py
@@ -150,12 +150,16 @@ def write_supercell(templatefile, supercell, filename, number):
     from a template.
     """
 
+    #Number of atoms in supercell
+    numatoms_sc = len(supercell['positions'][0, :])
+    
     tree = ElementTree.parse(templatefile)
     root = tree.getroot()
 
     for structure in root.iter('structure'):
         for crystal in structure.findall('crystal'):
-            crystal.attrib['scale'] = "1.0000"
+            #Write new scale
+            crystal.attrib['scale'] = "0.10000"
             #Remove existing unitcell lattice vectors
             for basevect in crystal.findall('basevect'):
                 crystal.remove(basevect)
@@ -163,32 +167,93 @@ def write_supercell(templatefile, supercell, filename, number):
             for i in range(3):
                 ElementTree.SubElement(crystal, 'basevect').text = \
                     '    '.join(map(str, supercell["lattvec"][:, i]))
+            
+            numspecies = 0
+            for species in structure.findall('species'):
+                element = species.attrib['speciesfile'].split(".")[0]
+                numspecies += 1
 
-    tree.write('test_output.xml')
+                #Remove existing unitcell basis
+                for atom in species.findall('atom'):
+                    species.remove(atom)
+
+                #Write supercell basis
+                for i in range(numatoms_sc):
+                    if(supercell['types'][i] == numspecies):
+                        atom = ElementTree.SubElement(species, 'atom')
+                        atom.set('coord', '    '.join(map(str, supercell["positions"][:, i])))
+
+    #Need python v>3.9 for this to work? The output without this will be tots uggo!
+    #ElementTree.indent(tree, '    ')
+
+    #tree.write(filename, pretty_print = True, encoding='UTF-8')
+    tree.write(filename)
+
+
+#TODO
+#def read_forces(filename):
+#    """
+#    Read a set of forces on atoms from filename, in
+#    exciting's output format. Units: eV/nm
+#    """
+    
 
 if __name__ == '__main__':
+    def usage():
+        """
+        Print an usage message and exit.
+        """
+        sys.exit("""Usage:
+\t{program:s} unitcell.in sow na nb nc cutoff[nm/-integer] supercell_template.in
+\t{program:s} unitcell.in reap na nb nc cutoff[nm/-integer]""".format(
+            program=sys.argv[0]))
 
-    #test
-    na = 2
-    nb = 2
-    nc = 2
-    nneigh = 2
-    action = "sow"
-    sfilename = "sc_input.xml"
-    ##
+    if len(sys.argv) not in (7, 8) or sys.argv[2] not in ("sow", "reap"):
+        usage()
+    ufilename = sys.argv[1]
+    action = sys.argv[2]
+    na, nb, nc = [int(i) for i in sys.argv[3:6]]
+    if action == "sow":
+        if len(sys.argv) != 8:
+            usage()
+        sfilename = sys.argv[7]
+    else:
+        if len(sys.argv) != 7:
+            usage()
+    if min(na, nb, nc) < 1:
+        sys.exit("Error: na, nb and nc must be positive integers")
+    if sys.argv[6][0] == "-":
+        try:
+            nneigh = -int(sys.argv[6])
+        except ValueError:
+            sys.exit("Error: invalid cutoff")
+        if nneigh == 0:
+            sys.exit("Error: invalid cutoff")
+    else:
+        nneigh = None
+        try:
+            frange = float(sys.argv[6])
+        except ValueError:
+            sys.exit("Error: invalid cutoff")
+        if frange == 0.:
+            sys.exit("Error: invalid cutoff")
     
-    print('Reading input.xml')
+    print("Reading {0}".format(ufilename))    
+
     unitcell_info = read_exciting_input('./')
+
     natoms = len(unitcell_info['types'])
+
     print('Analyzing the symmetries')
     symops = thirdorder_core.SymmetryOperations(
         unitcell_info["lattvec"], unitcell_info["types"], unitcell_info["positions"].T, SYMPREC)
     print("- Symmetry group {0} detected".format(symops.symbol))
     print("- {0} symmetry operations".format(symops.translations.shape[0]))
+
     print("Creating the supercell")
-    
     supercell = gen_supercell(unitcell_info, na, nb, nc)
     ntot = natoms * na * nb * nc
+
     print("Computing all distances in the supercell")
     dmin, nequi, shifts = calc_dists(supercell)
     if nneigh != None:
@@ -196,10 +261,12 @@ if __name__ == '__main__':
         print("- Automatic cutoff: {0} nm".format(frange))
     else:
         print("- User-defined cutoff: {0} nm".format(frange))
+
     print("Looking for an irreducible set of third-order IFCs")
     wedge = thirdorder_core.Wedge(unitcell_info, supercell, symops, dmin, nequi, shifts,
                                   frange)
     print("- {0} triplet equivalence classes found".format(wedge.nlist))
+
     list4 = wedge.build_list4()
     nirred = len(list4)
     nruns = 4 * nirred
@@ -211,3 +278,19 @@ if __name__ == '__main__':
             os.path.basename(sfilename)))
         write_supercell(sfilename, supercell,
                         "BASE.{0}".format(os.path.basename(sfilename)), 0)
+        width = len(str(4 * (len(list4) + 1)))
+        namepattern = "DISP.{0}.{{0:0{1}d}}".format(
+            os.path.basename(sfilename), width)
+        print("Writing displaced coordinates to DISP.{0}.*".format(
+            os.path.basename(sfilename)))
+        for i, e in enumerate(list4):
+            for n in xrange(4):
+                isign = (-1)**(n // 2)
+                jsign = -(-1)**(n % 2)
+                # Start numbering the files at 1 for aesthetic
+                # reasons.
+                number = nirred * n + i + 1
+                dsupercell = move_two_atoms(supercell, e[1], e[3], isign * H, e[0],
+                                          e[2], jsign * H)
+                filename = namepattern.format(number)
+                write_supercell(sfilename, dsupercell, filename, number)
